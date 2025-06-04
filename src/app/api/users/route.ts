c0caec5c6165a46165a262d8b7377edf86c54173
vincent <hkustliwenqian@hotmail.com>
2025-06-04 15:05:50 +0800

    Add files via upload

---
+++ src/app/api/users/route.ts
@@ -0,0 +1,20 @@
+export const runtime = 'edge';
+let counter = 1;
+export async function GET() {
+  // 模拟数据变化：每20秒返回不同的值
+  // if (Math.floor(Date.now() / 1000) % 10 < 10) {
+  //   res = { value: counter };
+  // } else {
+  //   counter += 1;
+  //   res = { value: counter };
+  // }
+
+  counter++;
+  const res = { value: counter }
+
+  console.log('counter', counter);
+  return new Response(JSON.stringify(res), {
+    status: 200,
+    headers: { 'Content-Type': 'application/json' }
+  });
+}
---
+++ src/app/users/[id]/page.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable */
+// app/posts/[id]/page.js
+
+export default async function PostPage({ params }: any) {
+  const id = (await params).id;
+  console.log('start fetch', id);
+  // 使用 fetch 在服务端获取数据（默认就是 Server Component）
+  const res = await fetch(`http://localhost:3000/api/users`, {
+    // 可选：设置缓存策略，Next.js 会结合 revalidate 使用
+    next: { revalidate: 10 } // ⚠️ 关键：设置 ISR 的重新验证时间为 10 秒
+  });
+
+  const post = await res.json();
+
+  return (
+    <main>
+      test ISR
+      <h1>{JSON.stringify(post)}</h1>
+      <hr />
+      <small>最后更新时间：{new Date().toLocaleString()}</small>
+    </main>
+  );
+}
